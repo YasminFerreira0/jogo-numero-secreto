454035753aa15a710421495b32e117800b5c6fce
Yasmin Ferreira <yasmincferreira8@gmail.com>
2022-12-31 18:39:15 -0400

    Update sorteioNumero.js

--- js/sorteioNumero.js
+++ js/sorteioNumero.js
@@ -1,7 +1,14 @@
+const menorValor = 1;
+const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio () {
-    return parseInt(math.random() * 100);
+    return parseInt(Math.random() * maiorValor + 1);
 }
-
 console.log('Número Secreto: ', numeroSecreto);
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
